6beefa8a60f2e53ca9ed0342dd66e1a39cb83f7f
Kaijie Zhou <kaijiez@bu.edu>
2020-02-05 12:09:07 -0500

    finalized p1

--- PS1/PS1.P1.js
+++ PS1/PS1.P1.js
@@ -1,14 +1,4 @@
 const sorted = string => {
-    const letters = string.split('');
-    //remove numbers and punctuations from string
-    const ans=[];
-    for(var i =0; i<letters.length; i++){
-        if(letters[i].toLowerCase()!=letters[i].toUpperCase()){
-            ans.push(letters[i])
-        }
-    }
-    //sort and join each character back
-    const result = ans.sort().join('');
-    return result
+    return string.split('').sort().join('');
 };
-console.log(` The sorted string is ${sorted('$#%5nrgg,,$@')}`);
+console.log(` The sorted string is ${sorted('supercalifragilisticexpialidocious')}`);
